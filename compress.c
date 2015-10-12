6104ebea6ff602cb4c6e57bb0ac4fe30a67d9b23
Steve Kondik <steve@cyngn.com>
2015-10-12 12:49:32 -1000

    tinycompress: Fix compilation on old targets
     * Exclude code for codec-specific data if kernel support is missing.
    
    Change-Id: I088e0244b01b01f21b80d9478407a185c34afbe8

--- compress.c
+++ compress.c
@@ -572,7 +572,7 @@ int compress_set_gapless_metadata(struct compress *compress,
 	return 0;
 }
 
-#ifdef ENABLE_EXTENDED_COMPRESS_FORMAT
+#if defined(SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM)
 int compress_set_next_track_param(struct compress *compress,
 	union snd_codec_options *codec_options)
 {
@@ -581,20 +581,15 @@ int compress_set_next_track_param(struct compress *compress,
 
 	if (ioctl(compress->fd, SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM, codec_options))
 		return oops(compress, errno, "cannot set next track params\n");
+	return 0;
+}
+#else
+int compress_set_next_track_param(struct compress *compress __unused,
+	union snd_codec_options *codec_options __unused)
+{
 	return 0;
 }
 #endif
-
-int compress_set_next_track_param(struct compress *compress,
-	union snd_codec_options *codec_options)
-{
-	if (!is_compress_running(compress))
-		return oops(compress, ENODEV, "device not ready");
-
-	if (ioctl(compress->fd, SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM, codec_options))
-		return oops(compress, errno, "cannot set next track params\n");
-	return 0;
-}
 
 bool is_codec_supported(unsigned int card, unsigned int device,
 		unsigned int flags, struct snd_codec *codec)
